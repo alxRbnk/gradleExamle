005c8ccbc74ae11bfd569d83d30d03877c8cdd3a
Alexey Rubnikovich <liftedfish@gmail.com>
2024-09-08 12:44:06 +0200

    fixed

--- src/main/java/by/rbnk/Main.java
+++
@@ -1,6 +0,0 @@
-package by.rbnk;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
--- src/test/java/by/rbnk/StringUtilsTest.java
+++ src/test/java/by/rbnk/StringUtilsTest.java
@@ -1,22 +1,24 @@
 package by.rbnk;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilsTest {
 
-    @Test
-    public void testIsPositiveNumber() {
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789"})
+    public void testIsPositiveNumber_positiveCases(String input) {
         StringUtils utils = new StringUtils();
-        assertTrue(utils.isPositiveNumber("123"));
-        assertFalse(utils.isPositiveNumber("-123"));
-        assertFalse(utils.isPositiveNumber("-123.45"));
-        assertFalse(utils.isPositiveNumber("-123,45"));
-        assertFalse(utils.isPositiveNumber("0"));
-        assertFalse(utils.isPositiveNumber("12a3"));
-        assertFalse(utils.isPositiveNumber(""));
-        assertFalse(utils.isPositiveNumber(null));
+        assertTrue(utils.isPositiveNumber(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-123", "-123.45", "-123,45", "0", "12a3", ""})
+    public void testIsPositiveNumber_negativeCases(String input) {
+        StringUtils utils = new StringUtils();
+        assertFalse(utils.isPositiveNumber(input));
     }
 }
